d73dc0d026a458058f19ced71145c81d3a9030b4
bouboumael <bouboumael@gmail.com>
2021-06-09 12:28:05 +0200

    resolve

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -47,13 +47,9 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        /*if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }*/
 
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('home'));
-        //throw new Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
