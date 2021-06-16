9f6f574f3d96262a0a9316c37ce894bb5958fb0f
bouboumael <bouboumael@gmail.com>
2021-06-16 11:12:23 +0200

    save

--- assets/app.js
+++ assets/app.js
@@ -7,10 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-
 import './bootstrap';
-
+import './loginAjax';
 
 require('bootstrap');
-
-import './loginAjax';
--- assets/loginAjax.js
+++ assets/loginAjax.js
@@ -1,16 +1,11 @@
-import { Modal } from 'bootstrap/dist/js/bootstrap.bundle.js';
+import { Modal } from 'bootstrap/dist/js/bootstrap.bundle';
 
-//const myModal = new Modal(document.getElementById('loginModal'));
-//myModal.show();
 const loginForm = document.querySelector('.login-button');
 loginForm.addEventListener('click', (event) => {
     event.preventDefault();
     const route = '/login';
     fetch(route)
-    .then((response) => response.text)
-    .then((html) => {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, 'text/html');
-        console.log(doc)
-    })
+        .then((response) => response.text)
+        .then((html) => {
+        });
 });
