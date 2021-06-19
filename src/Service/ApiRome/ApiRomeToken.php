554ea22c633e2e80f3495e4f844c6f3180cd648d
bouboumael <mael-chariault_student2021@wilder.school>
2021-06-20 01:16:32 +0200

    secure and change keys

--- src/Service/ApiRome/ApiRomeToken.php
+++ src/Service/ApiRome/ApiRomeToken.php
@@ -2,9 +2,9 @@
 
 namespace App\Service\ApiRome;
 
-use App\Service\ApiRome\ApiRomeConstants;
+use App\Service\ApiRome\ApiRomeConfig;
 
-class ApiRomeToken extends ApiRomeConstants
+class ApiRomeToken extends ApiRomeConfig
 {
     /**
      * Get Authorization Token for all request Pole Emploi
@@ -13,7 +13,7 @@ class ApiRomeToken extends ApiRomeConstants
      */
     protected function getToken(): string
     {
-        $response = $this->client->request(
+        $response = $this->getClient()->request(
             'POST',
             self::URL_TOKEN . 'connexion/oauth2/access_token?realm=%2Fpartenaire',
             [
@@ -22,12 +22,9 @@ class ApiRomeToken extends ApiRomeConstants
                 ],
                 'query' => [
                     'grant_type' => 'client_credentials',
-                    'client_id' => 'PAR_jobpermut_ceb5390408c34f309502ff23c3cf574703fe1771b153bba6610914bca60c46b1',
-                    'client_secret' => '6ed3f777a559c7e75b0c0bd2fd4a3cf23dd686940210a0907a0b3a573bd92fa0',
-                    'scope' => 'application_PAR_jobpermut_
-                    ceb5390408c34f309502ff23c3cf574703fe1771b153bba6610914bca60c46b1
-                    api_romev1
-                    nomenclatureRome'
+                    'client_id' => $this->getClientId(),
+                    'client_secret' => $this->getSecretKey(),
+                    'scope' => 'application_' . $this->getClientId() . ' api_romev1 nomenclatureRome'
                 ]
             ]
         );
