58d9d535a441aaa17d14fef60752e18217e462d7
RedPandore <tennessee-houry_student2021@wilder.school>
2021-07-06 11:53:06 +0200

    Resolve problem

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,18 @@ class CompanyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('address')
-            ->add('code')
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'JobPermut SARL'],
+                'label' => 'Nom de votre entreprise'
+                ])
+            ->add('address', TextType::class, [
+                'attr' => ['placeholder' => 'Orléans'],
+                'label' => 'Ville de l\'entreprise'
+                ])
+            ->add('code', NumberType::class, [
+                'attr' => ['placeholder' => '110294'],
+                'label' => 'Code d\'invitation'
+                ])
         ;
     }
 
