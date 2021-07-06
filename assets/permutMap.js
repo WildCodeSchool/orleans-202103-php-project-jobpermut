ff6802e2f3a9285607bf970e52d5c6af10790db5
Niurwa <raphaelbilletservoin@gmail.com>
2021-07-06 19:43:50 +0200

    add maps on view's div

--- assets/permutMap.js
+++ assets/permutMap.js
@@ -3,14 +3,14 @@ require('leaflet');
 // eslint-disable-next-line
 const L = window.L;
 
-let NbUser = ((document.getElementsByClassName('permutMap')).length)+1;
+const NbUser = ((document.getElementsByClassName('permutMap')).length) + 1;
 
 function initMap(
     lat = 48.852969,
     lon = 2.2,
 ) {
-    for (let i = 1; i < NbUser; i++) {
-        mapBefore = L.map(('map-before-' + i), {
+    for (let i = 1; i < NbUser; i += 1) {
+        const mapBefore = L.map((`map-before-${i}`), {
             zoomControl: false,
             scrollWheelZoom: false,
             dragging: false,
@@ -29,18 +29,18 @@ function initMap(
             },
         ).addTo(mapBefore);
 
-        mapAfter = L.map(('map-after-' + i), {
+        const mapAfter = L.map((`map-after-${i}`), {
             zoomControl: false,
             scrollWheelZoom: false,
             dragging: false,
-            tap: false, 
+            tap: false,
             touchZoom: false,
             center: [48.852969, 2.2],
         }).setView(
             [lat, lon],
             10,
         );
-        
+
         L.tileLayer(
             'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=99affac52963476dbbf2ed20db232af9',
             {
@@ -48,10 +48,9 @@ function initMap(
                 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
             },
         ).addTo(mapAfter);
-    };
+    }
 }
 
 window.onload = function () {
     initMap();
 };
-
