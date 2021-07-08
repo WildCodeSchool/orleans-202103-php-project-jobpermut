9e7d6818773581fd347f4cc96558c51c282e55fe
Wone <lochlainn.gadault@gmail.com>
2021-07-08 11:09:37 +0200

    style changes for permut compare

--- src/Controller/PermutSearchController.php
+++ src/Controller/PermutSearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Service\Direction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,23 @@ class PermutSearchController extends AbstractController
     /**
      * @Route("permutsearch", name="permutsearch")
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, Direction $direction): Response
     {
+        $homeCityCoordinate = [2.8884657, 48.9562018];
+        $workCityCoordinate = [2.3488, 48.8534];
+
+        $tripSummary1 = [];
+        $tripSummary2 = [];
+
+        $userWorkCoordinates = [2.765796, 48.878462];
+
+        $tripSummary1 = $direction->tripSummary($homeCityCoordinate, $workCityCoordinate);
+        $tripSummary2 = $direction->tripSummary($homeCityCoordinate, $userWorkCoordinates);
+
         return $this->render('permutsearch/index.html.twig', [
-            'users' => $userRepository->findAll(),
+            'user' => $userRepository->findOneById(1),
+            'tripSummary1' => $tripSummary1,
+            'tripSummary2' => $tripSummary2
         ]);
     }
 }
