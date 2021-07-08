6c3ff1f138b8256c478bc997056482f1ad368b79
Wone <lochlainn.gadault@gmail.com>
2021-07-08 16:06:26 +0200

    add informations permut compare and JS collapse for users cards

--- src/Controller/PermutSearchController.php
+++ src/Controller/PermutSearchController.php
@@ -15,21 +15,34 @@ class PermutSearchController extends AbstractController
      */
     public function index(UserRepository $userRepository, Direction $direction): Response
     {
-        $homeCityCoordinate = [2.8884657, 48.9562018];
-        $workCityCoordinate = [2.3488, 48.8534];
-
-        $tripSummary1 = [];
-        $tripSummary2 = [];
+        $homeCityCoordinate = [2.8884689, 48.9562087];
+        $workCityCoordinate = [2.3478, 48.8554];
 
         $userWorkCoordinates = [2.765796, 48.878462];
 
         $tripSummary1 = $direction->tripSummary($homeCityCoordinate, $workCityCoordinate);
         $tripSummary2 = $direction->tripSummary($homeCityCoordinate, $userWorkCoordinates);
 
+        $duration1 = 0;
+        $duration2 = 0;
+
+        if ($tripSummary1) {
+            $duration1 = (intval($tripSummary1['duration']['hours']) * 60) +
+                (intval($tripSummary1['duration']['minutes']));
+        }
+
+        if ($tripSummary2) {
+            $duration2 = (intval($tripSummary2['duration']['hours']) * 60) +
+                (intval($tripSummary2['duration']['minutes']));
+        }
+
+        $timeGained = $duration1 - $duration2;
+
         return $this->render('permutsearch/index.html.twig', [
             'users' => $userRepository->findby([], [], 5),
             'tripSummary1' => $tripSummary1,
-            'tripSummary2' => $tripSummary2
+            'tripSummary2' => $tripSummary2,
+            'timeGained' => $timeGained,
         ]);
     }
 }
