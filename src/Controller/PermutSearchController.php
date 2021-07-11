8a02e13fb69a3884e0622e654c08fc61f6bf5902
Niurwa <raphaelbilletservoin@gmail.com>
2021-07-11 15:17:50 +0200

    PermutSearchController ok

--- src/Controller/PermutSearchController.php
+++ src/Controller/PermutSearchController.php
@@ -3,10 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\Geocode;
 use App\Service\Direction;
-use App\Repository\UserRepository;
 use App\Repository\RegisteredUserRepository;
-use PhpParser\Node\Expr\Cast\Object_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,48 +15,70 @@ class PermutSearchController extends AbstractController
     /**
      * @Route("permutsearch", name="permutsearch")
      */
-    public function index(RegisteredUserRepository $regUserRepo, Direction $direction): Response
+    public function index(RegisteredUserRepository $regUserRepo, Direction $direction, Geocode $geocode): Response
     {
-        $rome = "";
+        $regUsersDatas = [];
 
-        if (!empty($this->getUser())) {
-            /** @var User */
-            $user = $this->getUser();
-            $user = $user->getRegisteredUser();
+        /** @var User */
+        $user = $this->getUser();
+        $user = $user->getRegisteredUser();
 
-            if ($user != null) {
-                $rome = $user->getRome();
-            }
+        if ($user != null) {
+            $rome = $user->getRome();
+            $userId = $user->getId();
         }
 
-        $homeCityCoordinate = [2.8884689, 48.9562087];
-        $workCityCoordinate = [2.3478, 48.8554];
-
-        $userWorkCoordinates = [2.765796, 48.878462];
-
+        $homeCityCoordinate = $geocode->getCoordinates($user->getCity());
+        $workCityCoordinate = $geocode->getCoordinates($user->getCityJob());
         $tripSummary1 = $direction->tripSummary($homeCityCoordinate, $workCityCoordinate);
-        $tripSummary2 = $direction->tripSummary($homeCityCoordinate, $userWorkCoordinates);
 
-        $duration1 = 0;
-        $duration2 = 0;
+        $userData = [ $userId => [
+            'homeCity' => $homeCityCoordinate,
+            'workCity' => $workCityCoordinate,
+            'tripSummary1' => $tripSummary1,
+            ]
+        ];
 
-        if ($tripSummary1) {
-            $duration1 = (intval($tripSummary1['duration']['hours']) * 60) +
-                (intval($tripSummary1['duration']['minutes']));
-        }
+        $usersRome = $regUserRepo->findby(['rome' => $rome], [], 5);
 
-        if ($tripSummary2) {
-            $duration2 = (intval($tripSummary2['duration']['hours']) * 60) +
-                (intval($tripSummary2['duration']['minutes']));
-        }
+        foreach ($usersRome as $regUser) {
+            $userHomeCoordinates = $geocode->getCoordinates($regUser->getCity());
+            $userWorkCoordinates = $geocode->getCoordinates($regUser->getCityJob());
+            $tripSummary2 = $direction->tripSummary($homeCityCoordinate, $userWorkCoordinates);
 
-        $timeGained = $duration1 - $duration2;
+            $duration1 = 0;
+            $duration2 = 0;
+
+            if ($tripSummary1) {
+                $duration1 = (intval($tripSummary1['duration']['hours']) * 60) +
+                    (intval($tripSummary1['duration']['minutes']));
+            }
+
+            if ($tripSummary2) {
+                $duration2 = (intval($tripSummary2['duration']['hours']) * 60) +
+                    (intval($tripSummary2['duration']['minutes']));
+            }
+
+            $timeGained = $duration1 - $duration2;
+
+            $regUsersDatas[$regUser->getId()] = [
+                'userHome' => $userHomeCoordinates,
+                'userWork' => $userWorkCoordinates,
+                'tripSummary2' => $tripSummary2,
+                'timeGained' => $timeGained,
+            ];
+
+            unset($regUsersDatas[$userId]);
+
+        };
+
+        dd($regUsersDatas);
+
 
         return $this->render('permutsearch/index.html.twig', [
             'users' => $regUserRepo->findby(['rome' => $rome], [], 5),
-            'tripSummary1' => $tripSummary1,
-            'tripSummary2' => $tripSummary2,
-            'timeGained' => $timeGained,
+            'userData' => $userData,
+            'regUsersData' => $regUsersDatas
         ]);
     }
 }
