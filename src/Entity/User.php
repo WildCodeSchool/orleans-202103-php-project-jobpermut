75f4bab9f9765d9bb8d438c4e0ef68423b24ddf3
bouboumael <bouboumael@gmail.com>
2021-07-13 13:40:20 +0200

    fix_serialize

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -125,15 +125,6 @@ class User implements UserInterface
         $this->userLikes = new ArrayCollection();
         $this->userLikedBy = new ArrayCollection();
     }
-    public function __serialize(): array
-    {
-        return [
-            'id' => $this->id,
-            'email' => $this->email,
-            'password' => $this->password,
-            'username' => $this->username,
-        ];
-    }
 
     public function getId(): ?int
     {
